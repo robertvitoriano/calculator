16c62d577dc5de62fdd7381e2ffe0a888e338082
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-10-10 10:01:02 -0300

    add operation sets as vector

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
+#include <cstdlib>
+#include <cstring>
 #include <ctype.h>
 #include <gtk/gtk.h>
+#include <vector> // Added include for vector
 
 GtkWidget *display;
 GtkWidget *window;
 GtkWidget *grid;
 char operation;
+
+struct OperationSet {
+  char operation;
+  int leftOperand;
+  int rightOperand;
+};
 
 void appendCharacterToDisplay(GtkButton *button) {
   const char *currentText = gtk_entry_get_text(GTK_ENTRY(display));
@@ -22,7 +31,6 @@ void onNumberClicked(GtkButton *button, gpointer userData) {
 
 void onOperationClicked(GtkButton *button, gpointer userData) {
   appendCharacterToDisplay(button);
-
   const char *data = (const char *)userData;
   operation = data[0];
 }
@@ -31,31 +39,100 @@ void onClearClicked(GtkButton *button, gpointer userData) {
   gtk_entry_set_text(GTK_ENTRY(display), "");
 }
 
+int getOperationResult(OperationSet *operationSet) {
+  int result;
+  switch (operationSet->operation) {
+  case '+':
+    result = operationSet->leftOperand + operationSet->rightOperand;
+    break;
+  case '-':
+    result = operationSet->leftOperand - operationSet->rightOperand;
+    break;
+  case '*':
+    result = operationSet->leftOperand * operationSet->rightOperand;
+    break;
+  case '/':
+    if (operationSet->rightOperand != 0) {
+      result = operationSet->leftOperand / operationSet->rightOperand;
+    } else {
+      printf("Error: Division by zero\n");
+      return 0;
+    }
+    break;
+  default:
+    printf("Unsupported operation: %c\n", operationSet->operation);
+    return 0;
+  }
+
+  return result;
+}
+
 void onEqualClicked(GtkButton *button, gpointer userData) {
   const char *currentText = gtk_entry_get_text(GTK_ENTRY(display));
   printf("Current text: %s\n", currentText);
 
-  int leftOperand = 0, rightOperand = 0;
-  char operationType;
-
-  sscanf(currentText, "%d%c%d", &leftOperand, &operationType, &rightOperand);
-
   int result = 0;
+  std::vector<OperationSet> operationSets;
 
-  switch (operationType) {
-  case '+':
-    result = leftOperand + rightOperand;
-    break;
-  case '-':
-    result = leftOperand - rightOperand;
-    break;
-  default:
-    printf("Unsupported operation: %c\n", operationType);
-    return;
+  int operandIndex = 0;
+  char leftOperand[8] = {'\0'};
+  char rightOperand[8] = {'\0'};
+  char operationType = '\0';
+  int rightOperatorsCounter = 0;
+
+  for (int i = 0; i < strlen(currentText); i++) {
+
+    if (operandIndex >= 7) {
+      operandIndex = 0;
+    }
+    if (leftOperand[0] != '\0' && operationType != '\0' &&
+        rightOperatorsCounter >= 1) {
+      if (rightOperand[0] == '\0') {
+        rightOperand[0] = 0;
+      }
+      int leftValue = std::atoi(leftOperand);
+      int rightValue = std::atoi(rightOperand);
+      operationSets.push_back({operationType, leftValue, rightValue});
+      printf("Left OPERAND %d", leftValue);
+      printf("Right OPERAND %d", rightValue);
+
+      memset(leftOperand, '\0', sizeof(leftOperand));
+      memset(rightOperand, '\0', sizeof(rightOperand));
+      operationType = '\0';
+      continue;
+    }
+    if (isdigit(currentText[i]) && operandIndex <= 7 && operationType == '\0') {
+      leftOperand[operandIndex] = currentText[i];
+      operandIndex++;
+      continue;
+    }
+    if (isdigit(currentText[i]) && operandIndex <= 7 &&
+        leftOperand[0] != '\0' && operationType != '\0') {
+      rightOperand[operandIndex] = currentText[i];
+      operandIndex++;
+      rightOperatorsCounter++;
+      continue;
+    }
+    if (!isdigit(currentText[i])) {
+      operationType = currentText[i];
+      continue;
+    }
+  }
+
+  std::vector<int> operationSetResults;
+  int finalResult = 0;
+  for (size_t i = 0; i < operationSets.size(); ++i) {
+    operationSetResults.push_back(getOperationResult(&operationSets[i]));
+  }
+
+  for (size_t i = 0; i < operationSetResults.size(); ++i) {
+    if (i > 0) {
+      finalResult += operationSetResults[i];
+    }
   }
 
   char resultText[256];
-  snprintf(resultText, sizeof(resultText), "%d", result);
+  snprintf(resultText, sizeof(resultText), "%d", finalResult);
   gtk_entry_set_text(GTK_ENTRY(display), resultText);
 }
 
@@ -107,9 +184,7 @@ int main(int argc, char *argv[]) {
   }
 
   g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
   gtk_widget_show_all(window);
-
   gtk_main();
 
   return 0;
