c700f90878fe7afd12592c502b69c789f497aee8
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-10-10 12:09:21 -0300

    refactor to use object oriented programmign

--- main.cpp
+++ main.cpp
@@ -2,168 +2,155 @@
 #include <cstring>
 #include <ctype.h>
 #include <gtk/gtk.h>
+#include <iostream>
 #include <vector>
 
-GtkWidget *display;
-GtkWidget *window;
-GtkWidget *grid;
-int finalResult = 0;
+class Calculator {
+private:
+  GtkWidget *display;
+  GtkWidget *window;
+  GtkWidget *grid;
+  int finalResult;
+  char currentOperation;
 
-char operation;
+  struct OperationSet {
+    char operation;
+    int leftOperand;
+    int rightOperand;
+  };
 
-struct OperationSet {
-  char operation;
-  int leftOperand;
-  int rightOperand;
-};
+public:
+  Calculator() : finalResult(0), currentOperation('\0') {}
 
-void appendCharacterToDisplay(GtkButton *button) {
-  const char *currentText = gtk_entry_get_text(GTK_ENTRY(display));
-  const char *buttonText = gtk_button_get_label(button);
+  void setupWindow(int *argc, char ***argv) {
+    gtk_init(argc, argv);
 
-  char newText[256];
-  snprintf(newText, sizeof(newText), "%s%s", currentText, buttonText);
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Object-Oriented Calculator");
+    gtk_window_set_default_size(GTK_WINDOW(window), 200, 300);
 
-  gtk_entry_set_text(GTK_ENTRY(display), newText);
-}
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
+    gtk_container_add(GTK_CONTAINER(window), vbox);
 
-void onNumberClicked(GtkButton *button, gpointer userData) {
-  appendCharacterToDisplay(button);
-}
+    display = gtk_entry_new();
+    gtk_box_pack_start(GTK_BOX(vbox), display, FALSE, FALSE, 2);
 
-void onOperationClicked(GtkButton *button, gpointer userData) {
-  appendCharacterToDisplay(button);
-  const char *data = (const char *)userData;
-  operation = data[0];
-}
+    grid = gtk_grid_new();
+    gtk_box_pack_start(GTK_BOX(vbox), grid, TRUE, TRUE, 2);
 
-void onClearClicked(GtkButton *button, gpointer userData) {
-  gtk_entry_set_text(GTK_ENTRY(display), "");
-}
+    setupButtons();
 
-int getOperationResult(const OperationSet *operationSet) {
-  switch (operationSet->operation) {
-  case '+':
-    finalResult += operationSet->leftOperand + operationSet->rightOperand;
-    break;
-  case '-':
-    finalResult += operationSet->leftOperand - operationSet->rightOperand;
-    break;
-  case '*':
-    finalResult *= operationSet->leftOperand * operationSet->rightOperand;
-    break;
-  case '/':
-    if (operationSet->rightOperand != 0) {
-      finalResult /= operationSet->leftOperand / operationSet->rightOperand;
-    } else {
-      printf("Error: Division by zero\n");
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_widget_show_all(window);
+    gtk_main();
+  }
+
+  static void onButtonClicked(GtkButton *button, gpointer userData) {
+    Calculator *calculator = static_cast<Calculator *>(userData);
+    const char *buttonText = gtk_button_get_label(button);
+
+    if (isdigit(buttonText[0]) || strchr("+-*/", buttonText[0])) {
+      calculator->appendCharacterToDisplay(buttonText);
+    } else if (strcmp(buttonText, "=") == 0) {
+      calculator->onEqualClicked();
+    } else if (strcmp(buttonText, "C") == 0) {
+      calculator->onClearClicked();
+    }
+  }
+
+  void appendCharacterToDisplay(const char *buttonText) {
+    const char *currentText = gtk_entry_get_text(GTK_ENTRY(display));
+    char newText[256];
+    snprintf(newText, sizeof(newText), "%s%s", currentText, buttonText);
+    gtk_entry_set_text(GTK_ENTRY(display), newText);
+  }
+
+  void onClearClicked() { gtk_entry_set_text(GTK_ENTRY(display), ""); }
+
+  void onEqualClicked() {
+    const char *currentText = gtk_entry_get_text(GTK_ENTRY(display));
+    std::vector<OperationSet> operationSets;
+
+    int leftOperand = 0;
+    int rightOperand = 0;
+    char operationType = '\0';
+
+    for (int i = 0; i < strlen(currentText); i++) {
+      if (isdigit(currentText[i])) {
+        int digit = currentText[i] - '0';
+        if (operationType == '\0') {
+          leftOperand = leftOperand * 10 + digit;
+        } else {
+          rightOperand = rightOperand * 10 + digit;
+        }
+      } else {
+        if (operationType != '\0') {
+          operationSets.push_back({operationType, leftOperand, rightOperand});
+          leftOperand = 0;
+          rightOperand = 0;
+        }
+        operationType = currentText[i];
+      }
+    }
+    if (operationType != '\0') {
+      operationSets.push_back({operationType, leftOperand, rightOperand});
+    }
+
+    finalResult = 0;
+    for (const auto &opSet : operationSets) {
+      finalResult = getOperationResult(opSet);
+    }
+
+    char resultText[256];
+    snprintf(resultText, sizeof(resultText), "%d", finalResult);
+    gtk_entry_set_text(GTK_ENTRY(display), resultText);
+  }
+
+  int getOperationResult(const OperationSet &operationSet) {
+    switch (operationSet.operation) {
+    case '+':
+      finalResult += operationSet.leftOperand + operationSet.rightOperand;
+      break;
+    case '-':
+      finalResult += operationSet.leftOperand - operationSet.rightOperand;
+      break;
+    case '*':
+      finalResult *= operationSet.leftOperand * operationSet.rightOperand;
+      break;
+    case '/':
+      if (operationSet.rightOperand != 0) {
+        finalResult /= operationSet.leftOperand / operationSet.rightOperand;
+      } else {
+        std::cerr << "Error: Division by zero\n";
+        return 0;
+      }
+      break;
+    default:
+      std::cerr << "Unsupported operation: " << operationSet.operation << "\n";
       return 0;
     }
-    break;
-  default:
-    printf("Unsupported operation: %c\n", operationSet->operation);
-    return 0;
+
+    return finalResult;
   }
 
-  return finalResult;
-}
+  void setupButtons() {
+    const char *buttons[4][4] = {{"+", "-", "*", "C"},
+                                 {"7", "8", "9", "/"},
+                                 {"4", "5", "6", "="},
+                                 {"1", "2", "3", "0"}};
 
-void onEqualClicked(GtkButton *button, gpointer userData) {
-  const char *currentText = gtk_entry_get_text(GTK_ENTRY(display));
-  printf("Current text: %s\n", currentText);
-
-  std::vector<OperationSet> operationSets;
-
-  int leftOperand = 0;
-  int rightOperand = 0;
-  char operationType = '\0';
-
-  for (int i = 0; i < strlen(currentText); i++) {
-    if (isdigit(currentText[i])) {
-      int convertedDigit = (currentText[i] - '0');
-      if (operationType == '\0') {
-        leftOperand = leftOperand * 10 + convertedDigit;
-      } else {
-        rightOperand = rightOperand * 10 + convertedDigit;
-      }
-    } else {
-
-      if (operationType != '\0') {
-        operationSets.push_back({operationType, leftOperand, rightOperand});
-        printf("Left OPERAND %d, Right OPERAND %d, Operation %c\n", leftOperand,
-               rightOperand, operationType);
-
-        leftOperand = 0;
-        rightOperand = 0;
-      }
-      operationType = currentText[i];
-    }
-  }
-
-  if (operationType != '\0') {
-    operationSets.push_back({operationType, leftOperand, rightOperand});
-  }
-
-  finalResult = 0;
-  for (const auto &opSet : operationSets) {
-    finalResult = getOperationResult(&opSet);
-    printf("\nFinal result %i\n", finalResult);
-  }
-
-  char resultText[256];
-  snprintf(resultText, sizeof(resultText), "%d", finalResult);
-  gtk_entry_set_text(GTK_ENTRY(display), resultText);
-}
-
-void setupWindow(int *argc, char ***argv) {
-  gtk_init(argc, argv);
-
-  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_title(GTK_WINDOW(window), "Simple Calculator");
-  gtk_window_set_default_size(GTK_WINDOW(window), 200, 300);
-
-  GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
-  gtk_container_add(GTK_CONTAINER(window), vbox);
-
-  display = gtk_entry_new();
-  gtk_box_pack_start(GTK_BOX(vbox), display, FALSE, FALSE, 2);
-
-  grid = gtk_grid_new();
-  gtk_box_pack_start(GTK_BOX(vbox), grid, TRUE, TRUE, 2);
-}
-
-int main(int argc, char *argv[]) {
-  setupWindow(&argc, &argv);
-
-  const char *buttons[5][4] = {{"+", "-", "*", "C"},
-                               {"7", "8", "9", "/"},
-                               {"4", "5", "6", "="},
-                               {"1", "2", "3", "0"}};
-
-  for (int i = 0; i < 5; ++i) {
-    for (int j = 0; j < 4; ++j) {
-      if (buttons[i][j] != NULL) {
+    for (int i = 0; i < 4; ++i) {
+      for (int j = 0; j < 4; ++j) {
         GtkWidget *button = gtk_button_new_with_label(buttons[i][j]);
         gtk_grid_attach(GTK_GRID(grid), button, j, i, 1, 1);
-
-        if (g_strcmp0(buttons[i][j], "C") == 0) {
-          g_signal_connect(button, "clicked", G_CALLBACK(onClearClicked), NULL);
-        } else if (g_strcmp0(buttons[i][j], "=") == 0) {
-          g_signal_connect(button, "clicked", G_CALLBACK(onEqualClicked), NULL);
-        } else if (isdigit(buttons[i][j][0])) {
-          g_signal_connect(button, "clicked", G_CALLBACK(onNumberClicked),
-                           NULL);
-        } else {
-          g_signal_connect(button, "clicked", G_CALLBACK(onOperationClicked),
-                           (gpointer)buttons[i][j]);
-        }
+        g_signal_connect(button, "clicked", G_CALLBACK(onButtonClicked), this);
       }
     }
   }
+};
 
-  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-  gtk_widget_show_all(window);
-  gtk_main();
-
+int main(int argc, char *argv[]) {
+  Calculator calculator;
+  calculator.setupWindow(&argc, &argv);
   return 0;
 }
